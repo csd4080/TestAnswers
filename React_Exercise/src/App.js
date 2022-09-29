96cad929e4eb5792786a839c5100466ba5fffafa
csd4080 <92384625+csd4080@users.noreply.github.com>
2022-09-29 21:56:06 +0300

    fixed css in react

--- React_Exercise/src/App.js
+++ React_Exercise/src/App.js
@@ -2,48 +2,47 @@
 import { useState } from 'react';
 import { useEffect } from "react";
 import { List } from 'semantic-ui-react';
-import { ListItemText } from '@mui/material';
 
+import './App.css';
 function App() {
 
- const [records,setRecords] = useState(0);
+  const [records, setRecords] = useState(0);
 
   useEffect(() => { //on render call for useEffect and fetch the data from api
-      fetch("https://jsonplaceholder.typicode.com/todos")
-        .then((response) => response.json())
-        .then((data)=>{
-          setRecords((data));  //insert the data in a state variable
-        })
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => {
+        setRecords((data));  //insert the data in a state variable
+      })
 
-    
+
   }, [records]);
 
   return (
     <div className="App">
-      <List style={{display: "inline-grid",textAlign: "center"}}>
-        { 
-          (records || [] ).map((item ) => ( 
-           <List.Item key = { item.id }  >
-            <br />
-            {item.completed === true && (  //if its completed give it a backround color of green
-              <ListItemText  style={{
-                background: "green",
-                color: "white",
-              }}>id: { item.id } : title: { item.title }</ListItemText>  
-            )}
+      <div className="Flex-box">
+        <List style={{ display: "inline-grid" }}>
+          {
+            (records || []).map((item) => (
+              <List.Item key={item.id}  >
+                <br />
+                {item.completed === true && (  //if its completed give it a backround color of green
+                  <li className="completed">  <br /> id: {item.id} 
+                  <br /> 
+                  title: {item.title}</li>
+                )}
 
-          {item.completed === false && (
-              <ListItemText  style={{
-                background: "red", //if its not completed give it a backround color of red
-                color: "white",
-              }}>id: { item.id } : title: { item.title }</ListItemText>  
-            )}  
-             
-              
+                {item.completed === false && (
+                 
+                  <li className="non-completed" >  <br />id: {item.id} 
+                  <br /> 
+                  title: {item.title}</li>)}
+
               </List.Item>
-          ))
-        }
-      </List>
+            ))
+          }
+        </List>
+      </div>
     </div>
   );
 }
